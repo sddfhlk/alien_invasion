9dc6860a8ae5b41f3b80083841be81a29864fbf8
yangqijian <yangqijian1@hotmail.com>
2021-09-17 17:20:48 +0800

    添加统计信息。。。

--- game_functions.py
+++ game_functions.py
@@ -4,6 +4,7 @@ from bullet import Bullet
 from alien import Alien
 from time import sleep
 
+
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
@@ -32,7 +33,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -41,9 +42,20 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def check_play_button(ai_settings,screen, stats, play_button,ship,aliens,bullets, mouse_x, mouse_y):
+    # 在玩家单击Play按钮时开始新游戏
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        #重置游戏统计信息
+        stats.reset_stats()
+        stats.game_active = True
+
+
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """更新屏幕上的图像，并且换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -53,6 +65,10 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+
+    # 如果游戏处于非活动状态，就绘制Play按钮
+    if not stats.game_active:
+        play_button.draw_button()
 
     # 让最近绘制的屏幕可见
     pygame.display.flip()
@@ -132,43 +148,46 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 
-def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
-    """响应飞船被外星人撞到"""
-    if stats.ships_left>0:
-        #将ships_left减1
-        stats.ships_left-=1
 
-        #清空外星人列表和子弹列表
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+    """响应飞船被外星人撞到"""
+    if stats.ships_left > 0:
+        # 将ships_left减1
+        stats.ships_left -= 1
+
+        # 清空外星人列表和子弹列表
         aliens.empty()
         bullets.empty()
 
-        #创建一群新的外星人，并将飞船放到屏幕底端中央
-        create_fleet(ai_settings,screen,ship,aliens)
+        # 创建一群新的外星人，并将飞船放到屏幕底端中央
+        create_fleet(ai_settings, screen, ship, aliens)
         ship.center_ship()
 
-        #暂停
+        # 暂停
         sleep(0.5)
 
     else:
-        stats.game_active=False
+        stats.game_active = False
 
-def check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets):
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
     """检查是否有外星人到达了屏幕底端"""
-    screen_rect=screen.get_rect()
+    screen_rect = screen.get_rect()
     for alien in aliens.sprites():
-        if alien.rect.bottom>=screen_rect.bottom:
-            #像飞船被撞到一样进行处理
-            ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+        if alien.rect.bottom >= screen_rect.bottom:
+            # 像飞船被撞到一样进行处理
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
             break
 
-def update_aliens(ai_settings,stats,screen, ship, aliens,bullets):
+
+def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """检查是否有外星人位于屏幕边缘，然后更新所有外星人的位置"""
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
 
     # 检查外星人和飞船之间的碰撞
     if pygame.sprite.spritecollideany(ship, aliens):
-        ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 
-    #检查是否有外星人到达屏幕底端
-    check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets)
+    # 检查是否有外星人到达屏幕底端
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
